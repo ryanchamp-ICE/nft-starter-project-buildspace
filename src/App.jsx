1315396f91b2b9fbb063c45164b604a4b85f7e17
Ryan Champ <ryan.champ@gmail.com>
2021-12-27 04:35:15 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,179 @@
+import React, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+import myEpicNft from './utils/MyEpicNFT.json';
+import './styles/App.css';
+import twitterLogo from './assets/twitter-logo.svg';
+
+// Constants
+const TWITTER_HANDLE = '_buildspace';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const MY_TWITTER_HANDLE = 'IceColdEdge';
+const MY_TWITTER_LINK = `https://twitter.com/${MY_TWITTER_HANDLE}`;
+const OPENSEA_LINK = '';
+const TOTAL_MINT_COUNT = 50;
+
+const CONTRACT_ADDRESS = "0xFA584b2Ea6E80c1Da51588731923C8439135a762";
+
+const App = () => {
+
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      // I wonder how Cardano Web3 wallets work?
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+
+    let chainId = await ethereum.request({ method: 'eth_chainId' });
+    console.log("Connected to chain: ", chainId);
+
+    const rinkebyChainId = "0x4";
+    if (chainId !== rinkebyChainId) {
+      alert("You are not connected to the Rinkeby Test Network!");
+    }
+
+    // check if we are authorized to access the users wallet
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    // User can have more than one authorized account
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found authorized account: ", account);
+      setCurrentAccount(account);
+      setupEventListener();
+    } else {
+      console.log("No authorized account found.");
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get metamask!");
+        return;
+      }
+
+      // This method requests access to accounts
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected: ", accounts[0]);
+      setCurrentAccount(accounts[0]);
+      setupEventListener();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const askContractToMintNft = async () => {
+    
+
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Metamask not found");
+        return;
+      }
+
+      // Cardano equivalent?
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      // Abstraction of an ethereum account used to sign/send transactions
+      const signer = provider.getSigner();
+      const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+      console.log("Going to pop wallet now to pay gas");
+      let nftTxn = await connectedContract.makeAnEpicNFT();
+
+      console.log("Mining... please wait.");
+      await nftTxn.wait();
+
+      console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const setupEventListener = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Metamask not found");
+        return;
+      }
+
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+      connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+        console.log(from, tokenId.toNumber());
+        alert(`Hey! We've minted your NFT and sent it to your wallet! Give it some time for all the data to show. Here is the link to check it out: https://rinkeby.rarible.com/token/${CONTRACT_ADDRESS}:${tokenId.toNumber()}`);
+      });
+
+      console.log("Event listener has been set up");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // Render Methods
+  const renderNotConnectedContainer = () => (
+    <button onClick={connectWallet} className="cta-button connect-wallet-button">
+      Connect to Wallet
+    </button>
+  );
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">My IceColdNFT Collection</p>
+          <p className="sub-text">
+            Each unique. Each beautiful. Discover your NFT today.
+          </p>
+          {currentAccount === "" ? (
+            renderNotConnectedContainer()
+          ) : (
+            <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+              Mint NFT
+            </button>
+          )}
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built on @${TWITTER_HANDLE}`}</a>
+
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={MY_TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built by @${MY_TWITTER_HANDLE}`}</a>
+
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
